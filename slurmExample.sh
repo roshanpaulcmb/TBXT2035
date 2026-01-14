0cec572a95d8a0158d701f1871a731332cb031ca
roshanpaulcmb <rop174@pitt.edu>
2026-01-13 19:05:37 -0500

    Updated slurmExample.sh

--- slurmExample.sh
+++ slurmExample.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#SBATCH --job icon_system2
-# send email about job start and end
-#SBATCH --mail-user=bka28@pitt.edu
+#SBATCH --job <jobName>
+#SBATCH --mail-user=<email>
 #SBATCH --mail-type=ALL
 
 #SBATCH --nodes=1
 #SBATCH --partition=dept_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint="L40"
-
 
 #run the MD in the conda environment "struct"
 conda run -n md python dcdTools.py
